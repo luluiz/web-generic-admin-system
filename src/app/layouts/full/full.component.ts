6ec7c35909714af019db3cdebd2090968a7fa2a3
luiz <luluizbezerra@gmail.com>
2020-08-22 16:22:53 -0300

    refits on full layout

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -1,46 +1,30 @@
-
 import { MediaMatcher } from '@angular/cdk/layout';
-import {
-    ChangeDetectorRef,
-    Component,
-    OnDestroy
-} from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { MatRadioChange } from '@angular/material/radio';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { Router } from '@angular/router';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 import { MenuItems } from '../../shared/menu-items/menu-items';
 import { AuthenticationService } from '../../shared/services/authentication.service';
 
-
-
-/** @title Responsive sidenav */
 @Component({
     selector: 'app-full-layout',
     templateUrl: 'full.component.html',
-    styleUrls: []
+    styleUrls: ['full.component.scss']
 })
 export class FullComponent implements OnDestroy {
-    mobileQuery: MediaQueryList;
-    dir = 'ltr';
-    green = false;
-    blue = false;
-    dark = false;
-    minisidebar = false;
-    boxed = false;
-    danger = false;
-    showHide = false;
-    url = '';
-    sidebarOpened = false;
-    status = false;
-
-    public showSearch = false;
-
+    public mobileQuery: MediaQueryList;
+    public green: boolean = false;
+    public blue: boolean = false;
+    public dark: boolean = false;
+    public minisidebar: boolean = false;
+    public boxed: boolean = false;
+    public danger: boolean = false;
+    public sidebarOpened: boolean = false;
+    public colors: string[] = ['Default (Blue)', 'Red', 'Teal Green', 'Dark Blue'];
+    public color: string = this.colors[0];
     public config: PerfectScrollbarConfigInterface = {};
     private _mobileQueryListener: () => void;
-
-    clickEvent() {
-        this.status = !this.status;
-    }
-
 
     constructor(
         public router: Router,
@@ -60,6 +44,27 @@ export class FullComponent implements OnDestroy {
         this.mobileQuery.removeListener(this._mobileQueryListener);
     }
 
+    changeDarkMode(evt: MatSlideToggleChange) {
+        const checked: boolean = evt.checked;
+        if (checked)
+            this.green = this.blue = this.danger = false;
+        this.dark = checked;
+    }
 
-    // Mini sidebar
+    changeColor(evt: MatRadioChange) {
+        const color: string = evt.value;
+
+        if (color == 'Red') {
+            this.green = this.blue = false;
+            this.danger = true;
+        } else if (color == 'Teal Green') {
+            this.danger = this.blue = false;
+            this.green = true;
+        } else if (color == 'Dark Blue') {
+            this.green = this.danger = false;
+            this.blue = true;
+        } else if (color == 'Default Blue') {
+            this.danger = this.green = this.blue = false;
+        }
+    }
 }
